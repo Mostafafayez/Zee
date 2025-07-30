e37af1529401e5dfebab4598ac15b383aa51d571
mostafafayez <fayezmostafa51@gmail.com>
2025-07-30 15:02:10 +0300

    Initial push to Zee repo

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -30,7 +30,7 @@ class AuthController extends Controller
         return response()->json([
             'token' => $token,
             'user' => $user,
-            'role' => $user->getRoleNames(),
+            'role' => $user->getRoleNames()->first(),
         ]);
     }
 
